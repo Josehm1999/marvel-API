5983548c6d93ba8691c538d3047d0ed77c9540f9
juangr18 <juanr970318@gmail.com>
2022-02-01 18:10:07 -0500

    Change and create file creators.html

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 const URLAPI =
-  "https://gateway.marvel.com:443/v1/public/characters?ts=1234&apikey=66d8867aa625aaf1d6c014d689ce7f7a&hash=37eaed289ef8407adcd1a85d4961e99f";
+  "https://gateway.marvel.com/v1/public/creators?ts=1234&apikey=66d8867aa625aaf1d6c014d689ce7f7a&hash=37eaed289ef8407adcd1a85d4961e99f";
 
 const getMarvel = (url) => {
   return fetch(url)
@@ -13,12 +13,18 @@ const getMarvel = (url) => {
 const getCharacters = (data) => {
   let html = ``;
   data.forEach((j) => {
-    let image=j.thumbnail.path +"."+j.thumbnail.extension;
+    let image = j.thumbnail.path + "." + j.thumbnail.extension;
+    let comics=j.comics.items;
     html += '<div class="col">';
     html += '<div class="card h-100">';
     html += `<img src="${image}" class="card-img-top" alt="...">`;
     html += '<div class="card-body">';
-    html+=`<h5 class="card-title">${j.name}</h5>`;
+    html += `<h5 class="card-title">First name: ${j.firstName}</h5>`;
+    html += `<h6 class="card-title">Full name: ${j.fullName}</h6>`;
+    html += `<p class="card-text">Amount comics: ${j.comics.available}</p>`;
+    html += `${comics.forEach((c)=>{
+        `<p class="card-text">${c.name}</p>`
+    })}`;
     html += "</div>";
     html += "</div>";
     html += "</div>";
